ffea32cf32eacfade4b47d65b51eaee52dda4847
Yogesh2899 <ygoyal2_be24@thapar.edu>
2025-08-11 15:24:51 +0530

    Add files via upload

---
+++ ASS2_Q4_PART_E.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void changeCase(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 'a' + 'A';
+        }
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] - 'A' + 'a';
+        }
+    }
+}
+
+int main()
+{
+    char ch[] = "YoGeSh";
+    changeCase(ch);
+    cout << ch << endl;
+}
