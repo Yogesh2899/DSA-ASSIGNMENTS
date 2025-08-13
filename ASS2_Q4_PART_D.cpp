8b4200119636129ad24500c59c1447254d2aa122
Yogesh2899 <ygoyal2_be24@thapar.edu>
2025-08-13 11:06:07 +0530

    Add files via upload

---
+++ ASS2_Q4_PART_D.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+void lowerCase(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] - 'A' + 'a';
+                }
+    }
+}
+void alphabetical(char str[])
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        count++;
+    }
+    int len = count;
+
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (str[i] > str[j])
+            {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    char str[] = "Yogesh";
+    lowerCase(str);
+    alphabetical(str);
+
+    cout << "Sorted string: " << str << endl;
+}
